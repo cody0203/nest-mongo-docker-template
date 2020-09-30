f6203065d4c576125dc20f7a4184182ebdb6c62c
pham.cong.dinh <pham.cong.dinh@b122333-mac.local>
2020-09-30 10:55:31 +0700

    Update docker for development and product, update env config

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,11 @@
-FROM node:12.13-buster as development
-
-ENV NODE_ENV=development
-ENV NODE_ENV=${NODE_ENV}
+FROM node:12.13-alpine as development
 
 WORKDIR /usr/src/app
 
-COPY package.json ./
-COPY yarn.lock ./
-
-RUN yarn --dev install
+COPY package.json .
+RUN npm install
 
 COPY . .
 
-RUN yarn build
-
-# seperate build for production
-FROM node:12.13-alpine as production
-
-ARG NODE_ENV=production
-ENV NODE_ENV=${NODE_ENV}
-
-WORKDIR /usr/src/app
-
-COPY package*.json ./
-
-RUN npm install --only=production
-
-COPY . .
-
-COPY --from=development /usr/src/app/dist ./dist
-
-CMD ["node", "dist/main"]
+EXPOSE 3000
+CMD [ "npm", "run", "start:prod" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,31 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ExampleModule } from './example/example.module';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
   imports: [
     AuthModule,
     ExampleModule,
     ConfigModule.forRoot({
-      envFilePath: ['.env.development', '.env.production'],
+      envFilePath: `.env.${ENV}`,
       isGlobal: true,
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const MONGODB_URI = configService.get<string>('MONGODB_URI');
+
+        return {
+          uri: MONGODB_URI,
+        };
+      },
       inject: [ConfigService],
     }),
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(ENV);
+  }
+}
